cc46d31be7142ff40247077ba9b817033fa31aaf
Vladimir Bykov <vladimir-bykov-1999@mail.ru>
2020-12-08 17:56:45 +0300

    no message

---
+++ Калейдоскоп/Box.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Калейдоскоп
+{
+    class Box
+    {
+        int sx = 1;
+        int sy = 1;
+        static int width, height;
+        Panel panel;
+        Graphics graph;
+
+        public enum Figure
+        {
+            line,circle
+        };
+
+        static Figure figure;
+        static int line_x1;
+        static int line_y1;
+        static int line_x2;
+        static int line_y2;
+        static Color color;
+        static Random rand=new Random();
+
+        static int circle_x;
+        static int circle_y;
+        static int circle_r;
+
+        public Box(Panel panel, int sx, int sy)
+        {
+            this.sx = sx;
+            this.sy = sy;
+            this.panel = panel;
+            graph=panel.CreateGraphics();
+        }
+
+        static public void setSize(int w, int h)
+        {
+            width = w;
+            height = h;
+        }
+
+        static public void choiceFigure()
+        {
+            int f = rand.Next(0, Enum.GetNames(typeof(Figure)).Length);
+            figure = (Figure)f;
+            color = Color.FromArgb(rand.Next(0, 256), rand.Next(0, 256), rand.Next(0, 256));
+            switch (figure)
+            {
+                case Figure.line:
+                    line_x1 = rand.Next(0, width);
+                    line_y1 = rand.Next(0, height);
+                    line_x2 = rand.Next(0, width);
+                    line_y2 = rand.Next(0, height);
+                    break;
+                case Figure.circle:
+                    circle_x = rand.Next(0, width);
+                    circle_y = rand.Next(0, height);
+                    circle_r = rand.Next(0, width / 4);
+                    break;
+            }
+        } 
+
+        private void drawLine()
+        {
+            Pen pen= new Pen(color);
+            graph.DrawLine(pen, cx(line_x1), cy(line_y1), cx(line_x2), cy(line_y2));
+        }
+
+        private void drawCircle()
+        {
+            Brush brush = new SolidBrush(color);
+            graph.FillEllipse(brush, 
+                cx(circle_x) - circle_r, 
+                cy(circle_y) - circle_r, 
+                circle_r * 2, 
+                circle_r*2);
+        }
+
+        public void drawFigure()
+        {
+            switch (figure)
+            {
+                case Figure.line:
+                    drawLine();
+                    break;
+                case Figure.circle:
+                    drawCircle();
+                    break;
+            }
+        }
+
+        private int cx(int x)
+        {
+            return sx == 1 ? x : width - x;
+        }
+
+        private int cy(int y)
+        {
+            return sy == 1 ? y : height - y;
+        }
+    }
+}
---
+++ Калейдоскоп/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Калейдоскоп
+{
+    public partial class Form1 : Form
+    {
+        Box [,] box;
+        int w, h;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+            timer.Enabled = true;
+        }
+
+        public void Init()
+        {
+            Box.setSize(panel1.Width, panel1.Height);
+            w = panel.Width / panel1.Width;
+            h = panel.Height / panel1.Height;
+            int sx, sy;
+            box = new Box[w, h];
+            for (int i=0;i<w; i++)
+                for (int j = 0; j < h; j++)
+                {
+                    Panel cell= new Panel();
+                    cell.Parent = panel;
+                    //cell.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+                    cell.Location = new System.Drawing.Point(i*panel1.Width,j*panel1.Height);
+                    cell.Size = new System.Drawing.Size(panel1.Width, panel1.Height);
+                    sx = (i % 2 == 1) ? -1 : 1;
+                    sy = (j % 2 == 1) ? -1 : 1;
+                    box[i,j] = new Box(cell,sx,sy);
+                }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Box.choiceFigure();
+            for (int i = 0; i < w; i++)
+                for (int j = 0; j < h; j++)
+                    box[i, j].drawFigure();
+        }
+    }
+}
